ad10e1c1c631ea30474616073927d86fb8e0d1ae
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2018-06-30 10:29:10 -0600

    tratando archivos

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 void agregarSeleccion();
 void listarSeleccion();
 void eliminarSeleccion();
+void agregarSeleccionF();
 //void abrirArchivo();
 lista list;
 
@@ -18,11 +19,12 @@ int main(){
 
     do{
         cout<<"Examen #2"<<endl;
-        cout<<"1.Agregar Seleccion: "<<endl;
+        cout<<"1.Agregar Seleccion Inicio: "<<endl;
         cout<<"2.Listar Seleccion: "<<endl;
         cout<<"3.Eliminar Seleccion: "<<endl;
-        cout<<"4.Abrir Archivo Binario: "<<endl;
-        cout<<"5.Salir: "<<endl;
+        cout<<"4.Agregar Seleccion al Final: "<<endl;
+        cout<<"5.Abrir Archivo Binario: "<<endl;
+        cout<<"6.Salir: "<<endl;
         cin>>opcion;
 
         switch(opcion){
@@ -39,10 +41,14 @@ int main(){
             break;
 
             case '4':
-            //abrirArchivo();
+            agregarSeleccionF();
             break;
 
             case '5':
+
+            break;
+
+            case '6':
             seguido=false;
             break;
             default:
@@ -82,6 +88,32 @@ void agregarSeleccion(){
 
 }
 
+void agregarSeleccionF(){
+    string nombre;
+    int pGanados, pPerdidos, pEmpatados, pAnotado;
+    string pGol;
+
+    cout<<"Ingrese el nombre de la seleccion a Jugar"<<endl;
+    cin>>nombre;
+    cout<<"Ingrese los partidos ganados"<<endl;
+    cin>>pGanados;
+    cout<<"Ingrese los partidos perdidos"<<endl;
+    cin>>pPerdidos;
+    cout<<"Ingrese los partidos empatados"<<endl;
+    cin>>pEmpatados;
+    cout<<"Ingrese los goles anotados"<<endl;
+    cin>>pAnotado;
+    cout<<"Ingrese los goles anotados por el jugador"<<endl;
+    cin>>pGol;
+    seleccion sele(nombre,pGanados, pPerdidos, pEmpatados, pAnotado,pGol);
+    Nodo n(sele);
+    list.agregarFinal(n);
+
+    ofstream fsalida("SeleccionF.bin", ios::out | ios::binary);
+    fsalida.write(reinterpret_cast<char*>(&sele),sizeof(seleccion*));
+    fsalida.close();
+
+}
 void listarSeleccion(){
         char opcion;
         cout<<"1.Selecciones más goleadores: "<<endl;
